782c008c16efcff2a27f83238dc4b05ffd8f4b52
Richard Simko <richard@waroncancer.com>
2022-12-01 11:25:56 +0100

    Fixed imports

--- dist/main.js
+++ dist/main.js
@@ -1,31 +1,51 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = __importDefault(require("@actions/core"));
-const github_1 = __importDefault(require("@actions/github"));
+const core = __importStar(require("@actions/core"));
+const github = __importStar(require("@actions/github"));
 async function run() {
     try {
         const { GITHUB_SHA, GITHUB_TOKEN } = process.env;
-        const tagName = core_1.default.getInput("tag_name");
+        const tagName = core.getInput("tag_name");
         if (!GITHUB_SHA) {
-            core_1.default.setFailed("Missing GITHUB_SHA");
+            core.setFailed("Missing GITHUB_SHA");
             return;
         }
         if (!GITHUB_TOKEN) {
-            core_1.default.setFailed("Missing GITHUB_TOKEN");
+            core.setFailed("Missing GITHUB_TOKEN");
             return;
         }
         if (!tagName) {
-            core_1.default.setFailed("Missing tag_name");
+            core.setFailed("Missing tag_name");
             return;
         }
-        const octokit = github_1.default.getOctokit(GITHUB_TOKEN);
+        const octokit = github.getOctokit(GITHUB_TOKEN);
         let ref;
         try {
             ref = await octokit.rest.git.getRef({
-                ...github_1.default.context.repo,
+                ...github.context.repo,
                 ref: `tags/${tagName}`,
             });
         }
@@ -39,21 +59,21 @@ async function run() {
         }
         if (!ref) {
             await octokit.rest.git.createRef({
-                ...github_1.default.context.repo,
+                ...github.context.repo,
                 ref: `refs/tags/${tagName}`,
                 sha: GITHUB_SHA,
             });
         }
         else {
             await octokit.rest.git.updateRef({
-                ...github_1.default.context.repo,
+                ...github.context.repo,
                 ref: `tags/${tagName}`,
                 sha: GITHUB_SHA,
             });
         }
     }
     catch (error) {
-        core_1.default.setFailed(error.message);
+        core.setFailed(error.message);
     }
 }
 run();
