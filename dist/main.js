5c6b135bc19abf1b9af3a12e49bbb72a3b0b784d
Richard Simko <richard@waroncancer.com>
2022-12-01 11:21:58 +0100

    Rewrote to TS

---
+++ dist/main.js
@@ -0,0 +1,56 @@
+import core from "@actions/core";
+import github from "@actions/github";
+const context = github.context;
+async function run() {
+    try {
+        const { GITHUB_SHA, GITHUB_TOKEN } = process.env;
+        const tagName = core.getInput("tag_name");
+        if (!GITHUB_SHA) {
+            core.setFailed("Missing GITHUB_SHA");
+            return;
+        }
+        if (!GITHUB_TOKEN) {
+            core.setFailed("Missing GITHUB_TOKEN");
+            return;
+        }
+        if (!tagName) {
+            core.setFailed("Missing tag_name");
+            return;
+        }
+        const octokit = github.getOctokit(GITHUB_TOKEN);
+        let ref;
+        try {
+            ref = await octokit.rest.git.getRef({
+                ...context.repo,
+                ref: `tags/${tagName}`,
+            });
+        }
+        catch (e) {
+            if (e.status === 404) {
+                // Ignore tag not existing
+            }
+            else {
+                throw e;
+            }
+        }
+        if (!ref) {
+            await octokit.rest.git.createRef({
+                ...context.repo,
+                ref: `refs/tags/${tagName}`,
+                sha: GITHUB_SHA,
+            });
+        }
+        else {
+            await octokit.rest.git.updateRef({
+                ...context.repo,
+                ref: `tags/${tagName}`,
+                sha: GITHUB_SHA,
+            });
+        }
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
+run();
+//# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,31 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import core from "@actions/core";
+import github from "@actions/github";
 const context = github.context;
 async function run() {
-
   try {
     const { GITHUB_SHA, GITHUB_TOKEN } = process.env;
-    const tagName = core.getInput('tag_name');
+    const tagName = core.getInput("tag_name");
     if (!GITHUB_SHA) {
-      core.setFailed('Missing GITHUB_SHA');
+      core.setFailed("Missing GITHUB_SHA");
       return;
     }
 
     if (!GITHUB_TOKEN) {
-      core.setFailed('Missing GITHUB_TOKEN');
+      core.setFailed("Missing GITHUB_TOKEN");
       return;
     }
 
     if (!tagName) {
-      core.setFailed('Missing tag_name');
+      core.setFailed("Missing tag_name");
       return;
     }
-
 
     const octokit = github.getOctokit(GITHUB_TOKEN);
     let ref;
     try {
-
-      ref = await octokit.git.getRef({
+      ref = await octokit.rest.git.getRef({
         ...context.repo,
-        ref: `tags/${tagName}`
+        ref: `tags/${tagName}`,
       });
     } catch (e) {
       if (e.status === 404) {
@@ -38,19 +35,18 @@ async function run() {
       }
     }
     if (!ref) {
-      await octokit.git.createRef({
+      await octokit.rest.git.createRef({
         ...context.repo,
         ref: `refs/tags/${tagName}`,
-        sha: GITHUB_SHA
+        sha: GITHUB_SHA,
       });
     } else {
-      await octokit.git.updateRef({
+      await octokit.rest.git.updateRef({
         ...context.repo,
         ref: `tags/${tagName}`,
-        sha: GITHUB_SHA
+        sha: GITHUB_SHA,
       });
     }
-
   } catch (error) {
     core.setFailed(error.message);
   }
