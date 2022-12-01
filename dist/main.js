79d05701e3ea622263a841a55e6d37c763d68bfd
Richard Simko <richard@waroncancer.com>
2022-12-01 11:22:51 +0100

    Fixed output modules

--- dist/main.js
+++ dist/main.js
@@ -1,23 +1,28 @@
-import core from "@actions/core";
-import github from "@actions/github";
-const context = github.context;
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = __importDefault(require("@actions/core"));
+const github_1 = __importDefault(require("@actions/github"));
+const context = github_1.default.context;
 async function run() {
     try {
         const { GITHUB_SHA, GITHUB_TOKEN } = process.env;
-        const tagName = core.getInput("tag_name");
+        const tagName = core_1.default.getInput("tag_name");
         if (!GITHUB_SHA) {
-            core.setFailed("Missing GITHUB_SHA");
+            core_1.default.setFailed("Missing GITHUB_SHA");
             return;
         }
         if (!GITHUB_TOKEN) {
-            core.setFailed("Missing GITHUB_TOKEN");
+            core_1.default.setFailed("Missing GITHUB_TOKEN");
             return;
         }
         if (!tagName) {
-            core.setFailed("Missing tag_name");
+            core_1.default.setFailed("Missing tag_name");
             return;
         }
-        const octokit = github.getOctokit(GITHUB_TOKEN);
+        const octokit = github_1.default.getOctokit(GITHUB_TOKEN);
         let ref;
         try {
             ref = await octokit.rest.git.getRef({
@@ -49,7 +54,7 @@ async function run() {
         }
     }
     catch (error) {
-        core.setFailed(error.message);
+        core_1.default.setFailed(error.message);
     }
 }
 run();
